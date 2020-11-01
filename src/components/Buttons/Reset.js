c93db5039a6516839711e4d537635e26495888c0
Wayne <kimjihyo0325@gmail.com>
2020-11-01 17:20:35 +0900

    lint fix

--- src/components/Buttons/Reset.js
+++ src/components/Buttons/Reset.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Button from '@material-ui/core/Button';
 
 const Reset = () => {
-    return (
-        <Button variant="contained" color="primary">Reset</Button>
-    );
+  return (
+    <Button variant="contained" color="primary">
+      Reset
+    </Button>
+  );
 };
 
 export { Reset };
