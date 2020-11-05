a42036c437881b6a30b6e023727a53ff61b9ee8b
spencerwilson123321 <swilson176@my.bcit.ca>
2020-11-05 11:16:29 -0800

    created code for parsing input text file

--- src/ai/input.js
+++ src/ai/input.js
@@ -1,16 +1,44 @@
+
+
+// handles input of text file to our state generator, passes the file text to 2 parsers.
 function readInputFile() {
+    // get input tag
     let input = document.getElementById('input');
+    // get file
     let file = input.files[0];
+    // create reader
     let reader = new FileReader();
 
+    // read file as text
     reader.readAsText(file);
-    
+
+    // when its done reading print to console.
     reader.onload = function(){
-        console.log(reader.result);
+        // split into lines
+        let lines = reader.result.split('\n');
+
+        // starting color is just first line
+        let startingColour = lines[0];
+
+        // pass marble coordinates as string into parser and get array of marble coordinates.
+        let marbleCoordinates = parseCoords(lines[1]);
+
+        // results
+        console.log("Starting colour", startingColour);
+        console.log("Coordinates", marbleCoordinates);
     }
+
+    // catch error
     reader.onerror = function(){
         console.log(reader.error);
     }
 };
+
+
+// Function to parse coordinates from an inputString of marble coordinates.
+function parseCoords(inputString){
+    let arrayOfCoords = inputString.split(',');
+    return arrayOfCoords;
+}
 
 export default readInputFile;
